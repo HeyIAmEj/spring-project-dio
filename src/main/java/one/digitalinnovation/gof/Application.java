53d0e94ff9fd6d230298459cd39cff39af252f0c
Everton José <everton7ej@gmail.com>
2022-07-31 18:22:15 -0300

    Update Application.java

--- src/main/java/one/digitalinnovation/gof/Application.java
+++ src/main/java/one/digitalinnovation/gof/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
 /**
  * Projeto Spring Boot gerado via Spring Initializr.
  * Os seguintes módulos foram selecionados:
+ * - Poderia usar lombok...
  * - Spring Data JPA
  * - Spring Web
  * - H2 Database
